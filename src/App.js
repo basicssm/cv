448bc514c281c6208d21191e1f42733c2d9294d1
Sergio Sánchez Morillo <sergio.sanchez.morillo.contractor@bbva.com>
2019-03-16 23:16:42 +0100

    [movies] Use new components and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/title.jsx';
+import Subtitle from './components/subtitle.jsx';
+import Paragraph from './components/paragraph.jsx';
 
 class App extends Component {
+  constructor(props){
+      super(props);
+
+      this.state = {
+        ...props.user
+      }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App dark">
+        <Title text={this.state.name} cursor/>
+        <Subtitle text={this.state.job}/>
+        <Paragraph text={this.state.bio} typing/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const user = {
+  name: 'Sergio Sánchez',
+  job: 'Fullstack developer',
+  bio: 'Lorem impusn isquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+  linkedin: 'sergiosanchezmorillo',
+  instagram: 'srgsnxz',
+  twitter: 'basic_10',
+  skills: [
+    {
+      name: 'javascript',
+      level: '99',
+      image: ''
+    },
+    {
+      name: 'php',
+      level: '99',
+      image: ''
+    }
+  ]
+};
+
+ReactDOM.render(<App user={user}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
